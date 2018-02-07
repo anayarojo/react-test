be582c5e004c3411c30d652bd855cd830502a383
Raul Anaya <ranaya@qualisys.mx>
2018-02-07 14:46:32 -0700

    Hola mundon!

---
+++ platzi-video/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const app = document.getElementById("app");
+
+const holaMundo = <h1>Hola mundon!</h1>;
+
+ReactDOM.render(holaMundo, app);
+
